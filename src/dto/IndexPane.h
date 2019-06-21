4238ff915d09adbdbd9b8cc6fc65484c8d1cc587
An7ar35 <eadavison@protonmail.com>
2019-06-21 21:07:40 +0200

    More functionalities ------------------------------------------------------- [dto::IndexPane] Docs placed in place.

--- src/dto/IndexPane.h
+++ src/dto/IndexPane.h
@@ -6,9 +6,29 @@
 
 namespace blogator::dto {
     struct IndexPane {
+        /*
+         * Maps the Articles to their html <input type="checkbox" ../> parent(s)
+         * K = YYYY or YYYYMM of an article
+         * V = Year or Month parent in the html date index
+         */
         typedef std::unordered_map<unsigned, HTML::LineIndex_t>            ParentDateLineMap_t;
+        /*
+         * Maps the Articles to relevant HTML lines in the pane
+         * K = Article position in the master index
+         * V = HTML line i where the article is referenced to by a <a></a> link
+         */
         typedef std::unordered_map<size_t, HTML::LineIndex_t>              ArticleLineMap_t;
+        /*
+         * Maps the Tags to their html <input type="checkbox" ../> parents
+         * K = Tag string
+         * V = Tag parent the html tag index
+         */
         typedef std::unordered_map<std::string, HTML::LineIndex_t>         ParentTagLineMap_t;
+        /*
+         * Maps the Articles to relevant HTML lines in the pane
+         * K = Article position in the master index
+         * V = Collection of HTML lines i where the article is referenced to by a <a></a> link
+         */
         typedef std::unordered_map<size_t, std::vector<HTML::LineIndex_t>> ArticlesLineMap_t;
 
         struct DateTree {
@@ -22,32 +42,6 @@ namespace blogator::dto {
             ArticlesLineMap_t  articles_line_map;
             ParentTagLineMap_t tag_line_map;
         } _tag_tree;
-
-
-//        //html content
-//        HTML _by_date_html;
-//        HTML _by_tags_html;
-//
-//        /*
-//         * Maps the Articles to relevant HTML lines in the pane
-//         * K = Article position in the master index
-//         * V = HTML line(s) i where the article is referenced to by a <a></a> link
-//         */
-//        std::unordered_map<size_t, HtmlLineIndex_t>              _html_date_index_map;
-//        std::unordered_map<size_t, std::vector<HtmlLineIndex_t>> _html_tags_index_map;
-//
-//        /*
-//         * Maps the Articles to their html <input type="checkbox" ../> parent(s)
-//         * K = YYYY or YYYYMM of an article
-//         * V = Year or Month parent in the html date index
-//         */
-//        std::unordered_map<unsigned, HtmlLineIndex_t> _parent_date_checkbox_map;
-//        /*
-//         * Maps the Tags to their html <input type="checkbox" ../> parents
-//         * K = Tag string
-//         * V = Tag parent the html tag index
-//         */
-//        std::unordered_map<std::string, HtmlLineIndex_t> _parent_tag_checkbox_map;
     };
 }
 
