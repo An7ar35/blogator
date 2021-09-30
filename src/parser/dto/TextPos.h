7c48c37941bb8b8aaa704df7e0dd8154777dfdf5
An7ar35 <eadavison@protonmail.com>
2021-09-30 13:23:40 +0200

    [parser::TextPos] Added output stream operator to class.

--- src/parser/dto/TextPos.h
+++ src/parser/dto/TextPos.h
@@ -2,11 +2,17 @@
 #define BLOGATOR_PARSER_DTO_TEXTPOS_H
 
 #include <string>
+#include <ostream>
 
 namespace blogator::parser {
     struct TextPos {
         TextPos();
         TextPos( size_t line, size_t col );
+
+        friend std::ostream & operator<<( std::ostream &os, const TextPos &pos ) {
+            os << pos.line << ":" << pos.col;
+            return os;
+        }
 
         bool operator ==( const TextPos &rhs ) const;
         bool operator !=( const TextPos &rhs ) const;
